ce1355901e96dbc7693fc40b7f804fbe48400dff
irfanfath <mirfanf7@gmail.com>
2021-01-14 14:05:20 +0700

    update layouting

--- src/Container/Home.js
+++ src/Container/Home.js
@@ -5,6 +5,8 @@ import hp from "../Assets/Images/splash1.png"
 import hpstep from "../Assets/Images/Gabung.png"
 import hp2 from "../Assets/Images/Splash2.png"
 import Step from "../Component/Step";
+import merchant from "../Assets/Images/merchant.jpg"
+import jastiper from "../Assets/Images/jastiper.jpg"
  
 class Home extends Component {
   render() {
@@ -13,7 +15,7 @@ class Home extends Component {
       <div className="centered hero-section">
           <div className="w-container">
             <div className="hero-subheading">Rindu makanan daerah kamu?</div>
-            <h1 className="hero-heading">JastipinAja!!</h1>
+            <h1 className="hero-heading">JastipinAja!</h1>
             <img className="app-store" src={apple} alt="" />
             <img className="play-store" src={android} alt="" />
           </div>
@@ -28,7 +30,7 @@ class Home extends Component {
               <div className="margin-step">
                   <div className="w-step">
                     <div className="hero-step">Unduh Aplikasinya</div>
-                    <p>Unduh Aplikasi JastipinAja di App Store (IOS) atau Play Store (Android)</p>
+                    <p>Unduh Aplikasi JastipinAja! di App Store (IOS) atau Play Store (Android)</p>
                     <img className="app-store" src={apple} alt="" />
                     <img className="play-store" src={android} alt="" />
                   </div>
@@ -57,22 +59,22 @@ class Home extends Component {
                 <div className="w-row">
                     <div className="w-col w-col-6">
                         <div className="transparent white-box">
-                            {/* <img
+                            <img
                                 className="fullwidth-image"
-                                src="http://uploads.webflow.com/551bfa383f9283057b14ce65/551c06cc488e81037b9ea59d_city-scape.jpg"
+                                src={merchant}
                                 alt=""
-                            /> */}
+                            />
                             <h3 className="white-text">Merchant</h3>
                             <p className="white-text">Buat kamu para pelaku usaha pemilik restaurant, rumah makan, warung makan atau yang berminat buka usaha jastip mari bergabung menjadi mitra Merchant kami.</p>
                         </div>
                     </div>
                     <div className="w-col w-col-6">
                         <div className="transparent white-box">
-                            {/* <img
+                            <img
                                 className="fullwidth-image"
-                                src="http://uploads.webflow.com/551bfa383f9283057b14ce65/551c0a7c488e81037b9ea709_photo-1416400639808-f41f0c149b09.jpg"
+                                src={jastiper}
                                 alt=""
-                            /> */}
+                            />
                             <h3 className="white-text">Jastiper</h3>
                             <p className="white-text">Buat para Jastiper yang rindu makanan daerah yuk JastipinAja!</p>
                         </div>
