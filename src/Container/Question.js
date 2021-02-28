2c7e61ba7b499fa4b50f6082dc9a5ad05400be33
irfanfath <mirfanf7@gmail.com>
2021-03-01 00:31:51 +0700

    add question

--- src/Container/Question.js
+++ src/Container/Question.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import logo from "../Assets/Images/logojastip.png"
 
 export default class Question extends Component {
     state = {
@@ -33,21 +34,35 @@ export default class Question extends Component {
 
     render(){
         return(
-            <div id="FAQ" className="section">
-                <div className="block-content">
-                    <h2 className="section-title">Term And Condition</h2>
-                        <input type="text" onChange={(e) => this.setState({nama: e.target.value})} />
+            <div className="section">
+                <img src={logo} alt="" />
+                    <h2 className="section-title">Question</h2>
+                        <input className="input-question" type="text" placeholder="Full Name" onChange={(e) => this.setState({nama: e.target.value})} />
                         <br/>
-                        <input type="text" onChange={(e) => this.setState({email: e.target.value})} />
+                        <input className="input-question" type="text" placeholder="Email Address" onChange={(e) => this.setState({email: e.target.value})} />
                         <br/>
-                        <input type="number" onChange={(e) => this.setState({no_ponsel: e.target.value})} />
+                        <input className="input-question" type="number" placeholder="Phone Number" onChange={(e) => this.setState({no_ponsel: e.target.value})} />
                         <br/>
-                        <input type="text" onChange={(e) => this.setState({pesan: e.target.value})}/>
+                        <textarea className="input-question"type="text" placeholder="Message" onChange={(e) => this.setState({pesan: e.target.value})}/>
                         <br/>
-                        <button onClick={this.handleSubmit}>Submit</button>
-                    <div>Can&#x27;t find an answer? <a href="mailto:knez.milos3@gmail.com">Email us</a></div>
-                </div>
+                        <button className="button-question" onClick={this.handleSubmit}>Submit</button>
+                <div className>Any Question? <a href="jastipinaja@aldeoz.com">Email us</a></div>
             </div>
+            // <div id="FAQ" className="section">
+            //     <div className="block-content">
+            //         <h2 className="section-title">Question</h2>
+            //             <input type="text" onChange={(e) => this.setState({nama: e.target.value})} />
+            //             <br/>
+            //             <input type="text" onChange={(e) => this.setState({email: e.target.value})} />
+            //             <br/>
+            //             <input type="number" onChange={(e) => this.setState({no_ponsel: e.target.value})} />
+            //             <br/>
+            //             <input type="text" onChange={(e) => this.setState({pesan: e.target.value})}/>
+            //             <br/>
+            //             <button onClick={this.handleSubmit}>Submit</button>
+            //         <div>Any Question? <a href="jastipinaja@aldeoz.com">Email us</a></div>
+            //     </div>
+            // </div>
         )
     }
 }
