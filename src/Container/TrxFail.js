c2099be7f440707a1e71a625e606be1721677084
irfanfath <mirfanf7@gmail.com>
2021-03-01 00:38:21 +0700

    Fixing Page

--- src/Container/TrxFail.js
+++ src/Container/TrxFail.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 import logo from "../Assets/Images/logojastip.png"
-import sukses from "../Assets/Images/success.png"
+import sukses from "../Assets/Images/failed.png"
  
 export default class TrxFail extends Component {
+    handleMove = () => {
+        this.props.history.push('/question')
+    }
+    
   render() {
     return (
       <div className="section-failed">
@@ -10,6 +14,7 @@ export default class TrxFail extends Component {
         <div className="text-success">
         <img src={sukses} className="logo-sukses" width="5%" alt="" /> <br/><br/>
             Transaksi Anda Gagal, Silahkan Hubungi Admin Kami</div>
+            <button className="button-question" onClick={this.handleMove}>Hubungi admin</button>
       </div>
     );
   }
